214c1ede2ba63123f53a0430723f568d7b6d1ade
Minh <ptminh2013@gmail.com>
2020-02-11 11:17:07 -0800

    finallize the minimax algorithm

--- tictactoeAgent.js
+++ tictactoeAgent.js
@@ -1,78 +1,9 @@
-// // Tic Tac Toe
-// var Agent = function () {
-
-// }
-
-// Agent.prototype.selectMove = function(board) {
-//     // console.log("X: " + "[" + board.X + "]");
-//     var bestScore = -Infinity;
-//     var bestMove = 0;
-//     for (var i = 1; i < 10; i++) {
-//         if (board.cellFree(i)) {
-//             var boardClone = board.clone();
-//             boardClone.move(i);
-//             // console.log("Current move: " + i);
-//             var score = minimax(boardClone, 0, false);
-//             console.log("Is Player One? " + board.playerOne + " " + score +  " At " + i);
-//             if (score > bestScore) {
-//                 bestScore = score;
-//                 bestMove = i;
-//             }
-
-//         } 
-//     }
-//     // console.log("BestMove: " + bestMove);
-    
-//     return bestMove;
-// }
-
-
-// function minimax(board, depth, isMaxPlayer) {
-
-//     if (board.gameOver() === 1) {
-//         return 10 - depth;
-//     } else if (board.gameOver() === 2) {
-//         return -10 + depth;
-//     } else if (board.gameOver() === 3) {
-//         return 0;
-//     }
-//     if (isMaxPlayer) {
-//         for (var i = 1; i < 10; i++) {
-//             if (board.cellFree(i)) {
-//                 var boardClone = board.clone();
-//                 boardClone.move(i);
-//                 bestVal = -Infinity;
-//                 value = minimax(boardClone, depth + 1, false);
-//                 bestVal = Math.max(bestVal, value);
-//                 return bestVal                 
-//             } 
-//         }
-
-//     }   
-//     else {
-//         for (var i = 1; i < 10; i++) {
-//             if (board.cellFree(i)) {
-//                 var boardClone = board.clone();
-//                 boardClone.move(i);
-//                 bestVal = Infinity;
-//                 value = minimax(boardClone, depth + 1, true);
-//                 bestVal = Math.min(bestVal, value); 
-//                 return bestVal                    
-//             }
-         
-//         }
-
-//     }     
-// }
-
 // Tic Tac Toe
 var Agent = function () {
 
 }
 
 Agent.prototype.selectMove = function(board) {
-    // console.log("X: " + "[" + board.X.length + "]");
-    
     if (board.playerOne) {
         var bestScore = -Infinity;
         var bestMove = 0;
@@ -81,7 +12,6 @@ Agent.prototype.selectMove = function(board) {
                 var boardClone = board.clone();
                 boardClone.move(i);
                 var score = minimax(boardClone, 0, false);
-                console.log("Is Player One? " + board.playerOne + " " + score +  " At " + i);
                 if (score > bestScore) {
                     bestScore = score;
                     bestMove = i;
@@ -99,8 +29,7 @@ Agent.prototype.selectMove = function(board) {
             if (board.cellFree(i)) {
                 var boardClone = board.clone();
                 boardClone.move(i);
-                var score = minimax(boardClone, 0, false);
-                // console.log("Is Player One? " + board.playerOne + " " + score +  " At " + i);
+                var score = minimax(boardClone, 0, true);
                 if (score < bestScore) {
                     bestScore = score;
                     bestMove = i;
@@ -124,30 +53,29 @@ function minimax(board, depth, isMaxPlayer) {
         return 0;
     }
     if (isMaxPlayer) {
+        var bestVal = -Infinity;
         for (var i = 1; i < 10; i++) {
             if (board.cellFree(i)) {
                 var boardClone = board.clone();
                 boardClone.move(i);
-                bestVal = -Infinity;
                 value = minimax(boardClone, depth + 1, false);
-                bestVal = Math.max(bestVal, value);
-                return bestVal                 
+                bestVal = Math.max(bestVal, value);               
             } 
         }
+        return bestVal  
 
     }   
     else {
+        var bestVal = Infinity;
         for (var i = 1; i < 10; i++) {
             if (board.cellFree(i)) {
                 var boardClone = board.clone();
                 boardClone.move(i);
-                bestVal = Infinity;
                 value = minimax(boardClone, depth + 1, true);
-                bestVal = Math.min(bestVal, value); 
-                return bestVal                    
-            }
-         
+                bestVal = Math.min(bestVal, value);                  
+            }       
         }
+        return bestVal   
 
     }     
 }
